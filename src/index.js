588870200556d6997ebcb8be19821dbd70dc91c7
Karyna Lohvina <karinalogvina0@gmail.com>
2019-10-12 15:17:58 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var bracketsConfig = bracketsConfig.map(function(bracketarr) {
+    return bracketarr.join('');
+  });
+    while (str.length>0) {
+      var stop = true;
+      for (var i=0; i<bracketsConfig.length; i++) {
+       var item = bracketsConfig[i];
+      if (str.includes(item)) {
+        str =  str.replace(item, ''); 
+          if (str.length == 0) {
+            return true;
+          }
+        stop=false;
+      } 
+    }  
+    if (stop) {
+      return false;
+    }
+  }
 }
